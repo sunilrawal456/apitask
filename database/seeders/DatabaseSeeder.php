bcd45a90f78eb59e812c56c166026edbdabf7c2f
sunilrawal456 <87895206+sunilrawal456@users.noreply.github.com>
2025-04-12 16:57:07 +0530

    Add files via upload

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+         // User::factory(1)->create();
+
+         User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' =>Hash::make('12345')
+        ]);
+    }
+}
